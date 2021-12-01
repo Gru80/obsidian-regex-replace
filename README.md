2a10e990631f25a47f6dc6025464be2ff2b72385
EDER Martin <Martin.EDER@frequentis.com>
2021-12-01 21:00:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Obsidian Regex Find/Replace
+This plugin opens a dialog to find and replace text in the currently opened file.
+Regular expressins might be used in the search term if this option is enabled.
+Either occurances in the actually selected text or in the whole file are replaced, depending on the configured option.
+
+## How to use
+1. Open the command palette and run "Find and Replace using regular expressions"
+2. Fill in the find and replace term in the text areas
+3. Choose any of the options:
+    - Replace using regular expressions
+    - Replace only within the current selection or in the whole file
+4. Hit "Replace All" to execute the command
---
+++ src/main.ts
@@ -0,0 +1,194 @@
+import {
+	App,
+	ButtonComponent,
+	Editor,
+	Modal,
+	Notice,
+	Plugin,
+	TextComponent,
+	ToggleComponent,
+} from "obsidian";
+
+const logger = (logString: string): void => {console.log ("RegexFR: " + logString)};
+
+export default class RegexFindReplacePlugin extends Plugin {
+	async onload() {
+		logger("Loading...");
+
+		this.addCommand({
+			id: "obsidian-regex-replace",
+			name: "Find and Replace using regular expressions",
+			editorCallback: (editor) => {
+				new FindAndReplaceModal(this.app, editor).open();
+			},
+		});
+	}
+
+	onunload() {
+		logger("Bye!");
+	}
+}
+
+class FindAndReplaceModal extends Modal {
+	constructor(app: App, editor: Editor) {
+		super(app);
+		this.shouldRestoreSelection = true;
+		this.editor = editor;
+	}
+
+	editor: Editor;
+
+	onOpen() {
+		let { contentEl, titleEl, editor, modalEl } = this;
+
+		modalEl.addClass("find-replace-modal");
+		titleEl.setText("Regex Find/Replace");
+
+		const rowClass = "row";
+		const divClass = "div";
+
+		const createInterfaceInputRow = (label: string,	placeholder: string,): TextComponent => {
+			const containerEl = document.createElement(divClass);
+			containerEl.addClass(rowClass);
+
+			const targetEl = document.createElement(divClass);
+			targetEl.addClass("input-wrapper");
+
+			const labelEl = document.createElement(divClass);
+			labelEl.addClass("input-label");
+			labelEl.setText(label);
+
+			containerEl.appendChild(labelEl);
+			containerEl.appendChild(targetEl);
+
+			const component = new TextComponent(targetEl);
+			component.setPlaceholder(placeholder);
+
+			contentEl.append(containerEl);
+			return component;
+		};
+
+		const findInputComponent = createInterfaceInputRow("Find:", "e.g. (.*)");
+		const replaceWithInputComponent = createInterfaceInputRow("Replace:","e.g. $1",);
+
+		const bcontainerEl = document.createElement(divClass);
+		bcontainerEl.addClass(rowClass);
+
+
+		const submitButtonTarget = document.createElement(divClass);
+		submitButtonTarget.addClass("button-wrapper");
+		submitButtonTarget.addClass(rowClass);
+
+		const cancelButtonTarget = document.createElement(divClass);
+		cancelButtonTarget.addClass("button-wrapper");
+		cancelButtonTarget.addClass(rowClass);
+
+		const submitButtonComponent = new ButtonComponent(submitButtonTarget);
+		const cancelButtonComponent = new ButtonComponent(cancelButtonTarget);
+		
+		cancelButtonComponent.setButtonText("Cancel");
+		cancelButtonComponent.onClick(() => {
+			logger("Action cancelled.");
+			this.close();
+		});
+
+
+		submitButtonComponent.setButtonText("Replace All");
+		submitButtonComponent.setCta();
+		submitButtonComponent.onClick(() => {
+			let resultString = "";
+			const search = new RegExp(findInputComponent.getValue(),"gm");
+			logger("search string: " + search)
+			//logger("text to process:")
+			//logger(editor.getValue());
+
+			if(!selToggleComponent.getValue()) {
+				logger("Full document");
+
+				editor.setValue(editor.getValue().replace(search, replaceWithInputComponent.getValue()));
+			}
+			else {
+				logger("Selection only");
+				const hitCount = (editor.getSelection().match(search) || []).length;
+				logger("hitCount: " + hitCount);
+				if (hitCount > 0) {
+					const replacementText = editor.getSelection().replace(
+									search,
+									replaceWithInputComponent.getValue())
+
+					const selectionStart = editor.getCursor("from");
+
+					editor.replaceSelection(replacementText);
+
+					// We re-select the selected text (just for nicer user experience)
+					editor.setSelection(
+						selectionStart,
+						editor.offsetToPos(
+							editor.posToOffset(selectionStart) + replacementText.length,
+						),
+					);
+					resultString = "Made " + hitCount + " replacements";
+				}
+				else {
+					resultString = "No match! Nothing changed.";
+				}
+			}
+			this.close();
+			new Notice(resultString)
+			
+		});
+
+		// Build toggle row for enable/disable regular expressions
+		const toggleRegexContainerEl = document.createElement(divClass);
+		toggleRegexContainerEl.addClass(rowClass);
+
+		const toggleRegexTarget = document.createElement(divClass);
+		toggleRegexTarget.addClass(rowClass);
+		const regToggleComponent = new ToggleComponent(toggleRegexTarget);
+		regToggleComponent.setTooltip("Enable/disable use of regular expressions");
+
+		const toggleRegexLabel = document.createElement(divClass);
+		toggleRegexLabel.addClass("check-label");
+		toggleRegexLabel.setText("Use regular expressions");
+
+		toggleRegexContainerEl.appendChild(toggleRegexLabel);
+		toggleRegexContainerEl.appendChild(toggleRegexTarget);
+
+		// Build toggle row for enable/disable replace in selection only
+		const toggleSelContainerEl = document.createElement(divClass);
+		toggleSelContainerEl.addClass(rowClass);
+
+		const toggleSelTarget = document.createElement(divClass);
+		toggleSelTarget.addClass(rowClass);
+		const selToggleComponent = new ToggleComponent(toggleSelTarget);
+		selToggleComponent.setTooltip("Replace only in occurances of the currently selected text");
+
+		const toggleSelLabel = document.createElement(divClass);
+		toggleSelLabel.addClass("check-label");
+		toggleSelLabel.setText("Replace only in selection");
+
+		// Set default to enable
+		regToggleComponent.setValue(true);
+		
+		// Not implemented right now
+		regToggleComponent.setDisabled(true);
+		selToggleComponent.setDisabled(true);
+
+
+		// Add childs
+		toggleSelContainerEl.appendChild(toggleSelLabel);
+		toggleSelContainerEl.appendChild(toggleSelTarget);
+
+		bcontainerEl.appendChild(submitButtonTarget);
+		bcontainerEl.appendChild(cancelButtonTarget);
+
+		contentEl.appendChild(toggleRegexContainerEl);
+		contentEl.appendChild(toggleSelContainerEl);
+		contentEl.appendChild(bcontainerEl);
+	}
+
+	onClose() {
+		let { contentEl } = this;
+		contentEl.empty();
+	}
+}
